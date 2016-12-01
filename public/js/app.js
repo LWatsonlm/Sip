f9076e048743a3c016b88b901639ab6db38d5141
Latoya Watson <latoyamwatson@gmail.com>
2016-12-01 16:03:58 -0500

    map shows but will not listen to css for height or width

--- public/js/app.js
+++ public/js/app.js
@@ -57,23 +57,103 @@ angular
   }
 
   function indexFunction(Drink, $state, $sce, $scope) {
-    console.log("index controller");
+  console.log("index controller");
     this.initMap = initMap
     function initMap() {
       console.log("can u see mE?");
       var map;
       window.onload = function() {
-        console.log(document.getElementById('map'));
         map = new google.maps.Map(document.getElementById('map'), {
           zoom: 2,
-          center: new google.maps.LatLng(38.889931,-77.009003),
-          mapTypeId: 'terrain'
+          center: new google.maps.LatLng(2.8,-187.3),
+          mapTypeId: 'terrain',
+          styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ]
         });
-        // Create a <script> tag and set the API URL as the source.
+        // Create a <script> tag and set the USGS URL as the source.
         var script = document.createElement('script');
-        script.src = 'http://localhost:3000/api/drinks/';
+        // This example uses a local copy of the GeoJSON stored at
+        // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+        script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
         document.getElementsByTagName('head')[0].appendChild(script);
-
         // Loop through the results array and place a marker for each
         // set of coordinates.
         window.eqfeed_callback = function(results) {
