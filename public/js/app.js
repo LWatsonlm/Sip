377505ec4f8d2a5109c9021d77f8974a1f7a3014
Latoya Watson <latoyamwatson@gmail.com>
2016-12-01 11:46:50 -0500

    working map-ish

--- public/js/app.js
+++ public/js/app.js
@@ -59,16 +59,36 @@ angular
   function indexFunction(Drink, $state, $sce, $scope) {
     console.log("index controller");
     this.initMap = initMap
-      function initMap() {
-        console.log(document.getElementById('map'))
-        var map;
-
+    function initMap() {
+      console.log("can u see mE?");
+      var map;
+      window.onload = function() {
+        console.log(document.getElementById('map'));
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8,
+          zoom: 2,
+          center: new google.maps.LatLng(38.889931,-77.009003),
+          mapTypeId: 'terrain'
         });
+        // Create a <script> tag and set the API URL as the source.
+        var script = document.createElement('script');
+        script.src = 'http://localhost:3000/api/drinks/';
+        document.getElementsByTagName('head')[0].appendChild(script);
 
+        // Loop through the results array and place a marker for each
+        // set of coordinates.
+        window.eqfeed_callback = function(results) {
+          console.log("does this even happen?");
+          for (var i = 0; i < results.features.length; i++) {
+            var coords = results.features[i].geometry.coordinates;
+            var latLng = new google.maps.LatLng(coords[1],coords[0]);
+            var marker = new google.maps.Marker({
+              position: latLng,
+              map: map
+            });
+          }
+        }
       }
+    }
     this.drinks = Drink.query()
     this.newDrink = new Drink()
     this.create = function () {
