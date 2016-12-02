0c11dfe4d2e7d445d18e130eb8bfb2932b17f433
Latoya Watson <latoyamwatson@gmail.com>
2016-12-02 13:58:27 -0500

    need to fix a bug. checking out a new branch

--- public/js/app.js
+++ public/js/app.js
@@ -24,6 +24,7 @@ angular
     "Drink",
     "$state",
     "$stateParams",
+    "$scope",
     showFunction
   ])
 
@@ -63,25 +64,105 @@ angular
     });
   }
 
-
   function indexFunction(Drink, $state, $sce, $scope) {
     let bars
     Drink.query({}, (response) => {
       bars = response
       let map = new google.maps.Map(document.getElementById('map'), {
         zoom: 14,
-        center: {lat: 38.9064227, lng: -77.0284656},
+        center: {lat: 38.9064227, lng: -77.0284656}, // the center of Thomas Circle, NW
+        title: 'Drink Specials in DC',
+        styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ]
       })
       bars.forEach((bar) => {
         new google.maps.Marker({
-          position: bar.location,
+          position: bar.location, // lat and lng defined in response from API
           map: map
         })
       })
     })
     this.drinks = Drink.query()
     this.newDrink = new Drink()
-    this.create = function () {
+    this.create = function () { // create new drink specials
       this.newDrink.$save().then(function(drink) {
         $state.go("show", {restaurant_name: drink.restaurant_name})
       })
@@ -89,9 +170,8 @@ angular
   }
 
 
-  function showFunction(Drink, $state, $stateParams) {
+  function showFunction(Drink, $state, $stateParams, $scope) { //show function. Will like to make a modal in near future
     console.log("show me the savings");
-    this.drink = Drink.get({restaurant_name: $stateParams.restaurant_name})
     this.update = function() {
       this.drink.$update({restaurant_name: $stateParams.restaurant_name})
     }
