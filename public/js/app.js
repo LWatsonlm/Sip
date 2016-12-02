fd0ea782ea946de0c2f35712625e5e25e9c836ed
Latoya Watson <latoyamwatson@gmail.com>
2016-12-02 01:49:34 -0500

    almost there

--- public/js/app.js
+++ public/js/app.js
@@ -40,6 +40,14 @@ angular
         controller: "indexController",
         controllerAs: "vm"
       })
+      .state("about", {
+        url:"/sip/about",
+        templateUrl: "assets/js/ng-views/about.html"
+      })
+      .state("contact", {
+        url: "/sip/contact",
+        templateUrl: "assets/js/ng-views/contact.html"
+      })
       .state("show", {
         url: "/sip/:restaurant_name",
         templateUrl: "assets/js/ng-views/show.html",
@@ -62,92 +70,12 @@ angular
       bars = response
       let map = new google.maps.Map(document.getElementById('map'), {
         zoom: 14,
-        center: {lat: 38.9072, lng: -77.0369}
+        center: {lat: 38.910356, lng: -77.0292211},
       })
       bars.forEach((bar) => {
         new google.maps.Marker({
           position: bar.location,
-          map: map,
-          styles: [
-            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-            {
-              featureType: 'administrative.locality',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'geometry',
-              stylers: [{color: '#263c3f'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#6b9a76'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry',
-              stylers: [{color: '#38414e'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#212a37'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#9ca5b3'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry',
-              stylers: [{color: '#746855'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#1f2835'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#f3d19c'}]
-            },
-            {
-              featureType: 'transit',
-              elementType: 'geometry',
-              stylers: [{color: '#2f3948'}]
-            },
-            {
-              featureType: 'transit.station',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'geometry',
-              stylers: [{color: '#17263c'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#515c6d'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.stroke',
-              stylers: [{color: '#17263c'}]
-            }
-          ]
+          map: map
         })
       })
     })
