f3d64d8207c0d296314e722a10080374df3d6996
Latoya Watson <latoyamwatson@gmail.com>
2016-12-14 13:56:44 -0500

    checking on this

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var hbs           = require("hbs")
 
 var app = express()
 var Drink = mongoose.model("Drink")
-var port = process.env.PORT || 3001
+// var port = process.env.PORT || 3001
 
 app.set('port', (process.env.PORT || 5000));
 app.set("view engine", "hbs");
